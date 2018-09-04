c25e0ad2b4d209110bb0332dcc651ce6a0724c2f
Bobby <tobbe.pogen@live.se>
2018-09-04 11:06:49 +0200

    Source added

---
+++ RipTag/Source.cpp
@@ -0,0 +1,22 @@
+#include <Windows.h>
+
+#if _DEBUG
+#include <iostream>
+//Allocates memory to the console
+void _alocConsole() {
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	AllocConsole();
+	FILE* fp;
+	freopen_s(&fp, "CONOUT$", "w", stdout);
+}
+#endif
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+#if _DEBUG
+	_alocConsole();
+#endif
+
+	std::cout << "hello world" << std::endl;
+	return 0;
+}
