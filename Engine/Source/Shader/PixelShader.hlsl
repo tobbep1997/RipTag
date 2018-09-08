749cd66c181961d453baeaca4f96bd8de4c1c7cc
Unknown <tobbe.pogen@live.se>
2018-09-08 12:10:14 +0200

    Shaders is abit more correct

--- Engine/Source/Shader/PixelShader.hlsl
+++ Engine/Source/Shader/PixelShader.hlsl
@@ -1,11 +1,14 @@
 
 struct VS_OUTPUT
 {
-	float4 pos : SV_POSITION;
-	float4 color : COLOR;
+    float4 pos : SV_POSITION;
+    float4 worldPos : WORLD;
+    float4 normal : NORMAL;
+    float4 tangent : TANGENT;
+    float2 uv : UV;
 };
 
 float4 main(VS_OUTPUT input) : SV_TARGET
 {
-	return input.color;
+	return input.normal;
 }
--- Engine/Source/Shader/VertexShader.hlsl
+++ Engine/Source/Shader/VertexShader.hlsl
@@ -9,7 +9,7 @@ cbuffer EXAMPLE_BUFFER : register(b0)
 struct VS_INPUT
 {
 	float4 pos : POSITION;
-	float4 color : COLOR;
+	float4 normal : NORMAL;
 	float4 tangent : TANGENT;
 	float2 uv : UV;
 };
@@ -17,7 +17,10 @@ struct VS_INPUT
 struct VS_OUTPUT
 {
 	float4 pos : SV_POSITION;
-	float4 color : COLOR;
+    float4 worldPos : WORLD;
+	float4 normal : NORMAL;
+    float4 tangent : TANGENT;
+    float2 uv : UV;
 };
 
 VS_OUTPUT main(VS_INPUT input)
@@ -27,6 +30,9 @@ VS_OUTPUT main(VS_INPUT input)
 	
 
 	output.pos = mul(input.pos, mul(worldMatrix, mul(view,  projection)));
-	output.color = input.color;
+    output.worldPos = mul(input.pos, worldMatrix);
+    output.normal = mul(input.normal, worldMatrix);
+    output.tangent = mul(input.tangent, worldMatrix);
+    output.uv = input.uv;
 	return output;
 }
