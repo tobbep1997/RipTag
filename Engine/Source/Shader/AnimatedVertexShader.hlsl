4e15961708797c7b43831aafd0c0905117b9776c
Elias Finoli <elle_finoli@hotmail.com>
2018-09-20 15:54:55 +0200

    Fixed some issues in anim vertex shader.
    Importer/Exporter still needs to be looked at

--- Engine/Source/Shader/AnimatedVertexShader.hlsl
+++ Engine/Source/Shader/AnimatedVertexShader.hlsl
@@ -11,7 +11,7 @@ cbuffer CAMERA_BUFFER : register(b1)
 
 cbuffer SKINNING_BUFFER : register(b2)
 {
-	row_major float4x4 skinningMatrices[256];
+	float4x4 skinningMatrices[256];
 };
 
 struct VS_INPUT
@@ -55,22 +55,24 @@ VS_OUTPUT main(VS_INPUT input)
 	//Blend verts
 	float3 position = float3(0.0f, 0.0f, 0.0f);
 	float3 nor = float3(0.0f, 0.0f, 0.0f);
-
+	float3 tan = float3(0.0, 0.0, 0.0);
 	for (int i = 0; i < 4; i++)
 	{
-		if (input.jointinfluences[i] >= 0) //unused bone indices are negative
+		if (input.jointinfluences[i] <= 129) //unused bone indices are negative
 		{
 			//TODO: cbuffer gWorld
-			position += weights[i] * mul(float4(input.pos.xyz, 1.0f), skinningMatrices[input.jointinfluences[i]]).xyz;
+			position += weights[i] * mul(float4(input.pos.xyz, 1.0f), skinningMatrices[input.jointinfluences[i] - 1]).xyz;
 			nor += weights[i] * mul(input.normal,
-				skinningMatrices[input.jointinfluences[i]]).xyz;
+				skinningMatrices[input.jointinfluences[i] - 1]).xyz;
+			tan += weights[i] * mul(input.tangent,
+				skinningMatrices[input.jointinfluences[i] - 1]).xyz;
 		}
 	}
 
 	output.pos = mul(float4(position, 1.0f), mul(worldMatrix, viewProjection));
 	output.worldPos = mul(float4(position, 1.0f), worldMatrix);
-	output.normal = mul(input.normal, worldMatrix);
-	output.tangent = mul(input.tangent, worldMatrix);
+	output.normal = mul(float4(nor, 0.0), worldMatrix);
+	output.tangent = mul(float4(tan, 0.0f), worldMatrix);
 	output.uv = input.uv;
 	return output;
 }
