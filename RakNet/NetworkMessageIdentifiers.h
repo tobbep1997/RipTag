176580431dd20b2e57884d5f5c7f0a9dfd91f0fc
Max <max.tlatlik94@gmail.com>
2018-10-03 11:40:32 -0700

    Senaste för Mange

--- RakNet/NetworkMessageIdentifiers.h
+++ RakNet/NetworkMessageIdentifiers.h
@@ -43,8 +43,8 @@ namespace Network
 #pragma pack(push, 1)
 	struct ENTITY_MOVE_MESSAGE
 	{
-		unsigned char useTimeStamp; // Assign ID_TIMESTAMP to this
-		RakNet::Time timeStamp; // Put the system time in here returned by RakNet::GetTime() or some other method that returns a similar value
+		//unsigned char useTimeStamp; // Assign ID_TIMESTAMP to this
+		//RakNet::Time timeStamp; // Put the system time in here returned by RakNet::GetTime() or some other method that returns a similar value
 		unsigned char id; // Our network defined enum types
 		RakNet::NetworkID networkId;
 		float x, y, z; // Character position
@@ -57,8 +57,8 @@ namespace Network
 			y = _y;
 			z = _z;
 			networkId = _networkId;
-			useTimeStamp = ID_TIMESTAMP;
-			timeStamp = RakNet::GetTime();
+			//useTimeStamp = ID_TIMESTAMP;
+			//timeStamp = RakNet::GetTime();
 		}
 	};
 #pragma pack(pop)
