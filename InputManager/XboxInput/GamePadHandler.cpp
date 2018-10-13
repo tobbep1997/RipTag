7983b2ba39788e05561616790e3c7461ef11d9fe
Max <max.tlatlik94@gmail.com>
2018-10-12 17:08:30 -0700

    Started to expose GamePadHandler
    Is not finished

--- InputManager/XboxInput/GamePadHandler.cpp
+++ InputManager/XboxInput/GamePadHandler.cpp
@@ -182,6 +182,16 @@ DirectX::XMFLOAT2 GamePadHandler::GetLeftStickposition()
 	return DirectX::XMFLOAT2(0,0);
 }
 
+std::tuple<float, float> GamePadHandler::GetLeftStickPosLUA()
+{
+	if (m_state.IsConnected())
+	{
+
+		return std::tuple<float, float>(m_state.thumbSticks.leftX, m_state.thumbSticks.leftY);
+	}
+	return std::tuple<float,float>(0, 0);
+}
+
 DirectX::XMFLOAT2 GamePadHandler::GetRightStickposition()
 {
 	if (m_state.IsConnected())
@@ -189,6 +199,16 @@ DirectX::XMFLOAT2 GamePadHandler::GetRightStickposition()
 		return DirectX::XMFLOAT2(m_state.thumbSticks.rightX, m_state.thumbSticks.rightY);
 	}
 	return DirectX::XMFLOAT2(0, 0);
+}
+
+std::tuple<float, float> GamePadHandler::GetRightStickPosLUA()
+{
+	if (m_state.IsConnected())
+	{
+
+		return std::tuple<float, float>(m_state.thumbSticks.rightX, m_state.thumbSticks.rightY);
+	}
+	return std::tuple<float, float>(0, 0);
 }
 
 bool GamePadHandler::IsLeftStickPressed()
@@ -405,6 +425,19 @@ void GamePadHandler::SetVibration(const float& left, const float& right, const f
 		m_rightMotorVibration = right;
 		m_leftTriggerMotorVibration = leftTrigger;
 		m_rightTriggerMotorVibration = rightTrigger;
+	}
+}
+
+void GamePadHandler::REGISTER_TO_LUA()
+{
+	static bool isRegged = false;
+
+	if (!isRegged)
+	{
+		LUA::LuaTalker * talker = LUA::LuaTalker::GetInstance();
+		
+		
+
 	}
 }
 
--- InputManager/XboxInput/GamePadHandler.h
+++ InputManager/XboxInput/GamePadHandler.h
@@ -1,6 +1,43 @@
 #pragma once
 #include "GamePad/GamePad.h"
 #include <DirectXMath.h>
+#include <LuaTalker.h>
+#include <tuple>
+
+
+//LUA DEFINES
+#define LUA_GAMEPAD "Gamepad"
+#define LUA_GAMEPAD_RESUME "Resume"
+#define LUA_GAMEPAD_SUSPEND "Suspend"
+#define LUA_GAMEPAD_IS_CONNECTED "IsConnected"
+#define LUA_GAMEPAD_IS_A_PRESSED "IsAPressed"
+#define LUA_GAMEPAD_IS_B_PRESSED "IsBPressed"
+#define LUA_GAMEPAD_IS_Y_PRESSED "IsYPressed"
+#define LUA_GAMEPAD_IS_X_PRESSED "IsXPressed"
+#define LUA_GAMEPAD_GET_LEFT_STICK_Y "GetLeftStickY"
+#define LUA_GAMEPAD_GET_LEFT_STICK_X "GetLeftStickX"
+#define LUA_GAMEPAD_GET_RIGHT_STICK_Y "GetRightStickY"
+#define LUA_GAMEPAD_GET_RIGHT_STICK_X "GetRightStickX"
+#define LUA_GAMEPAD_GET_LEFT_STICK_POS "GetLeftStickPos"
+#define LUA_GAMEPAD_GET_RIGHT_STICK_POS "GetRightStickPos"
+#define LUA_GAMEPAD_IS_LEFT_STICK_PRESSED "IsLeftStickPressed"
+#define LUA_GAMEPAD_IS_RIGHT_STICK_PRESSED "IsRightStickPressed"
+#define LUA_GAMEPAD_IS_LT_PRESSED "IsLTPressed"
+#define LUA_GAMEPAD_IS_RT_PRESSED "IsRTPressed"
+#define LUA_GAMEPAD_GET_LT_VALUE "GetLTValue"
+#define LUA_GAMEPAD_GET_RT_VALUE "GetRTValue"
+#define LUA_GAMEPAD_IS_DP_LEFT_PRESSED "IsDPLeftPressed"
+#define LUA_GAMEPAD_IS_DP_RIGHT_PRESSED "IsDPRightPressed"
+#define LUA_GAMEPAD_IS_DP_UP_PRESSED "IsDPUpPressed"
+#define LUA_GAMEPAD_IS_DP_DOWN_PRESSED "IsDPDownPressed"
+#define LUA_GAMEPAD_IS_LB_PRESSED "IsLBPressed"
+#define LUA_GAMEPAD_IS_RB_PRESSED "IsRBPressed"
+#define LUA_GAMEPAD_SET_LEFT_VIBRATION "SetLeftVibration"
+#define LUA_GAMEPAD_SET_RIGHT_VIBRATION "SetRightVibration"
+#define LUA_GAMEPAD_SET_VIBRATION "SetVibration"
+#define LUA_GAMEPAD_SET_VIBRATION_LT_RT "SetVibrationLTRT"
+
+
 class GamePadHandler
 {
 private:
@@ -39,7 +76,9 @@ public:
 	static float GetRightStickXPosition();
 
 	static DirectX::XMFLOAT2 GetLeftStickposition();
+	static std::tuple<float, float> GetLeftStickPosLUA();
 	static DirectX::XMFLOAT2 GetRightStickposition();
+	static std::tuple<float, float> GetRightStickPosLUA();
 
 	static bool IsLeftStickPressed();
 	static bool IsRightStickPressed();
@@ -62,4 +101,6 @@ public:
 	static void SetRightVibration(const float & left);
 	static void SetVibration(const float & left, const float & right);
 	static void SetVibration(const float & left, const float & right, const float & leftTrigger, const float & rightTrigger);
+
+	static void REGISTER_TO_LUA();
 };
