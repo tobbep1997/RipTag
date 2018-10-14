beb49d58fb2d4c0b7000471eaddea3644b695d63
Unknown <tobbe.pogen@live.se>
2018-10-14 12:09:03 +0200

    Clean now boys

--- Engine/EngineSource/3D Engine/Extern.h
+++ Engine/EngineSource/3D Engine/Extern.h
@@ -17,29 +17,22 @@ namespace Shaders
 
 class Drawable;
 class VisibilityComponent;
-//class Guard;
 
 namespace DX {
 	extern ID3D11Device*			g_device;
 	extern ID3D11DeviceContext1*	g_deviceContext;
-	   
 	extern void SafeRelease(IUnknown * u);
 	extern Shaders::ShaderManager g_shaderManager;
 
 	extern std::vector<Drawable*> g_geometryQueue;
 	extern std::vector<Drawable*> g_animatedGeometryQueue;
-	extern std::vector<PointLight*> g_lights;
-
-	//Might be changed
 	extern std::vector<Drawable*> g_visabilityDrawQueue;
-
 	extern std::vector<Drawable*> g_wireFrameDrawQueue;
 
-	//TODO:: TEMP AFFF
+	extern std::vector<PointLight*> g_lights;
 	extern std::vector<VisibilityComponent*> g_visibilityComponentQueue;
 
 }
-//Tesla was here
 namespace Manager
 {
 	extern MeshManager g_meshManager;
