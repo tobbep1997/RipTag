0b36e90cf06af73ce88a758070e3c6cee32dfb07
Joakim <joakim.trossvik@gmail.com>
2018-10-26 15:39:58 +0200

    Name correction

--- RipTag/Source/Game/Abilities/Disable/DisableAbility.cpp
+++ RipTag/Source/Game/Abilities/Disable/DisableAbility.cpp
@@ -25,8 +25,8 @@ void DisableAbility::Init()
 	Transform::setPosition(-999.0f, -999.0f, -999.0f);
 
 	PhysicsComponent::setUserDataBody(this);
-	this->getBody()->SetObjectTag("Enemy");
-
+	this->getBody()->SetObjectTag("Disable");
+	
 }
 
 void DisableAbility::Update(double deltaTime)
@@ -46,6 +46,7 @@ void DisableAbility::Use()
 	case DisableAbility::Moving:
 		DirectX::XMFLOAT4A position = Transform::getPosition();
 		position = { -999.0f, -999.0f, -999.0f, 1.0f };
+		Transform::setPosition(position);
 		m_dState = DisableAbility::Wait;
 		break;
 	}
@@ -91,56 +92,18 @@ void DisableAbility::_logic(double deltaTime)
 			bool cont = false;
 			for (auto contact : RipExtern::m_contactListener->GetBeginContacts())
 			{
-				/*if (contact->GetShapeA()->GetBody()->GetUserData() == this)
+				if (contact->GetShapeA()->GetBody()->GetObjectTag() == "Disable")
 				{
-					std::cout << "self" << std::endl;
-					
-				}
-*/
-				if (contact->GetShapeA()->GetBody()->GetObjectTag() == "Dis")
-				{
-					if (contact->GetShapeB()->GetBody()->GetObjectTag() == "Eny")
+					if (contact->GetShapeB()->GetBody()->GetObjectTag() == "Enemy")
 					{
 						Enemy * temp = static_cast<Enemy*>(contact->GetShapeB()->GetBody()->GetUserData());
 						temp->DisableEnemy();
+						//break;
 					}
 				}
-				//std::cout << contact->GetShapeA()->GetBody()->GetUserData() << std::endl;
-				//std::cout << contact->GetShapeB()->GetBody()->GetUserData() << std::endl;
-				//if (static_cast<DisableAbility*>(contact->GetShapeA()->GetBody()->GetUserData()) != nullptr)
-				//{
-				//	//dynamic_cast<DisableAbility*>(contact->GetShapeA()->GetBody()->GetUserData());
-				//	void* v = contact->GetShapeB()->GetBody()->GetUserData();
-				//	if (v == nullptr || (long long)v == 0xFFFFFFFFCCCCCCCC)
-				//		std::cout << "FAK" << std::endl;
-				//	Enemy * temp = static_cast<Enemy*>(contact->GetShapeB()->GetBody()->GetUserData());
-				//	Enemy * oo = temp->validate();
-				//	if (oo && (long long)v != 0xFFFFFFFFCCCCCCCC)
-				//	{
-				//		
-				//		//RipExtern::lol = oo;
-				//		oo->DisableEnemy();
-				//		//std::cout << contact->GetShapeA()->GetBody()->GetUserData() << std::endl;
-				//		//std::cout << contact->GetShapeB()->GetBody()->GetUserData() << std::endl;
-				//		break;
-				//	}
-				//	
-				//}
-				/*if (static_cast<Enemy*>(contact->GetShapeA()->GetBody()->GetUserData()))
-				{
-					if (static_cast<DisableAbility*>(contact->GetShapeB()->GetBody()->GetUserData()))
-					{
-						
-						cont = true;
-						break;
-					}
-				}*/
 				
 			}
-			/*if (cont == true)
-			{
-				
-			}*/
+			
 			break;
 		}
 	
