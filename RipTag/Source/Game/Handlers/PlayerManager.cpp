a7876019a93ff051907836e868a867f55573b5da
Elias Finoli <elle_finoli@hotmail.com>
2018-11-27 14:42:44 +0100

    Fixed local player textures, also no longer floating

--- RipTag/Source/Game/Handlers/PlayerManager.cpp
+++ RipTag/Source/Game/Handlers/PlayerManager.cpp
@@ -156,6 +156,8 @@ void PlayerManager::CreateLocalPlayer(DirectX::XMFLOAT4A pos)
 		//this->setModel(Manager::g_meshManager.getSkinnedMesh("PLAYER1"));
 		//mLocalPlayer->setModel(Manager::g_meshManager.getStaticMesh("SPHERE"));
 		mLocalPlayer->setModel(Manager::g_meshManager.getSkinnedMesh("PLAYER1"));
+		mLocalPlayer->setTexture(Manager::g_textureManager.getTexture("PLAYER1"));
+		mLocalPlayer->setModelTransform(DirectX::XMMatrixTranslation(0.0, -1.7f, 0.0f));
 		{
 			auto animationPlayer = mLocalPlayer->getAnimationPlayer();
 
@@ -167,7 +169,6 @@ void PlayerManager::CreateLocalPlayer(DirectX::XMFLOAT4A pos)
 		}
 		mLocalPlayer->setScale(1.0f * 0.5625f, 1.0f* 0.5625f, 1.0f* 0.5625f);
 		mLocalPlayer->setPosition(0.0f, 0.0f, 0.0f);
-		mLocalPlayer->setTexture(Manager::g_textureManager.getTexture("SPHERE"));
 		mLocalPlayer->setTextureTileMult(2, 2);
 	}
 }
