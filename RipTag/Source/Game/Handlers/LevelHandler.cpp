4e00bb3a004dcbcd4b56cda26a30ec5ea4f2be6e
Joakim <joakim.trossvik@gmail.com>
2018-11-27 14:36:25 +0100

    lol xD gitBlame mangeman

--- RipTag/Source/Game/Handlers/LevelHandler.cpp
+++ RipTag/Source/Game/Handlers/LevelHandler.cpp
@@ -162,7 +162,7 @@ const unsigned short LevelHandler::getNextRoom() const
 void LevelHandler::_LoadCorrectRoom(const int& seed, const int& roomIndex)
 {
 	Room * room;
-	if (roomIndex == -1 || true)
+	if (roomIndex == -1 )
 	{
 		room = m_roomGenerator.getGeneratedRoom(m_worldPtr, 1, m_playerPtr);
 		room->setLoaded(true);
