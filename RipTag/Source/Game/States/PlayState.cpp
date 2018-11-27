aa95be8ff876935d081c22904c4e58cf77300969
Elias Finoli <elle_finoli@hotmail.com>
2018-11-27 14:28:16 +0100

    Player1 textures

--- RipTag/Source/Game/States/PlayState.cpp
+++ RipTag/Source/Game/States/PlayState.cpp
@@ -661,6 +661,7 @@ void PlayState::_loadTextures()
 	Manager::g_textureManager.loadTextures("FIRE");
 	Manager::g_textureManager.loadTextures("GUARD");
 	Manager::g_textureManager.loadTextures("ARMS");
+	Manager::g_textureManager.loadTextures("PLAYER1");
 
 
 }
