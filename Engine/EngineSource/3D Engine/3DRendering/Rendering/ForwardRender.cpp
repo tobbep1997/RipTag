eccb630e0d66a2db4d4df280f7f1435031502d47
Gustav <gustav.pettersson96@hotmail.com>
2018-12-07 09:58:32 +0100

    Nu

--- Engine/EngineSource/3D Engine/3DRendering/Rendering/ForwardRender.cpp
+++ Engine/EngineSource/3D Engine/3DRendering/Rendering/ForwardRender.cpp
@@ -350,7 +350,7 @@ void ForwardRender::AnimatedGeometryPass(Camera & camera)
 			m_destroyBuffer.TimerAndForwardVector = camera.getForward();
 			m_destroyBuffer.TimerAndForwardVector.w = DX::g_animatedGeometryQueue[i]->getDestructionRate();
 			m_destroyBuffer.lastPos = DX::g_animatedGeometryQueue[i]->getLastTransform();
-			
+			m_destroyBuffer.typeOfAbility = DX::g_animatedGeometryQueue[i]->getTypeOfAbilityUsed();
 			DirectX::XMStoreFloat4x4A(&m_destroyBuffer.worldMatrixInverse, DirectX::XMMatrixInverse(nullptr, DirectX::XMLoadFloat4x4A(&DX::g_animatedGeometryQueue[i]->getWorldmatrix())));
 			m_destroyBuffer.worldMatrix = DX::g_animatedGeometryQueue[i]->getWorldmatrix();
 			DXRHC::MapBuffer(m_destructionBuffer, &m_destroyBuffer, sizeof(DestroyBuffer), 0, 1, ShaderTypes::geometry);
--- Engine/EngineSource/Shader/Shaders/DestructionPixelShader.hlsl
+++ Engine/EngineSource/Shader/Shaders/DestructionPixelShader.hlsl
@@ -15,7 +15,7 @@ struct GS_OUTPUT
 
 float4 main(GS_OUTPUT input) : SV_TARGET
 {
-	
+	return float4(1,1,1,1);
 	VS_OUTPUT inin;
 	inin.pos = input.pos;
 	inin.worldPos = input.worldPos;
