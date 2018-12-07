4c03f5d107b8bc0a7c39e6ddc82390ff44210ce1
Frogge <frogge_9_1_@hotmail.com>
2018-12-07 09:27:08 +0100

    Removed spammy cout

--- RipTag/Source/Game/Actors/RemotePlayer.cpp
+++ RipTag/Source/Game/Actors/RemotePlayer.cpp
@@ -241,8 +241,6 @@ void RemotePlayer::_onNetworkAnimation(Network::ENTITYANIMATIONPACKET * data)
 		this->m_currentPitch = data->pitch;
 		this->m_currentPeek = data->peek;
 		this->setRotation(data->rot);
-
-		std::cout << m_currentSpeed << std::endl;
 	}
 }
 
